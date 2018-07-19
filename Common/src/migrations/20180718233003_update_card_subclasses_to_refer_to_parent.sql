bb3169027629ce70004cb4dd66687089512d1cc8
Ender <shfitzy@gmail.com>
2018-07-18 23:42:42 -0500

    Added migration to update Card subclass tables to reference the Card class to ensure a 1-1 relationship.

---
+++ Common/src/migrations/20180718233003_update_card_subclasses_to_refer_to_parent.sql
@@ -0,0 +1,7 @@
+ALTER TABLE item_cards ADD card_id int NOT NULL;
+
+ALTER TABLE equipment_cards ADD card_id int NOT NULL;
+
+ALTER TABLE gambit_cards ADD card_id int NOT NULL;
+
+ALTER TABLE character_cards ADD card_id int NOT NULL;
