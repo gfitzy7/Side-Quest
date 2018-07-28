bfc2efaf45bde4e9aeb09cf3a87ad111c8152c71
gfitzy7 <shfitzy@gmail.com>
2018-07-28 00:34:21 -0500

    New migration to create rarity levels.

---
+++ Common/src/migrations/20180728000118_create_rarity_levels.sql
@@ -0,0 +1,11 @@
+INSERT INTO rarities (name, priority) VALUES ('Common', 1);
+
+INSERT INTO rarities (name, priority) VALUES ('Uncommon', 2);
+
+INSERT INTO rarities (name, priority) VALUES ('Rare', 3);
+
+INSERT INTO rarities (name, priority) VALUES ('Epic', 4);
+
+INSERT INTO rarities (name, priority) VALUES ('Legendary', 5);
+
+INSERT INTO rarities (name, priority) VALUES ('Mythical', 6);
